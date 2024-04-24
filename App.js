765b3aad6e43486b5800f79164a9e74ebd6e9e2b
lucas <evangelistalucas034@gmail.com>
2024-04-23 22:05:09 -0300

    commit inicial

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Olá!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f46",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    color: "blue",
+    fontSize: 200,
   },
 });
